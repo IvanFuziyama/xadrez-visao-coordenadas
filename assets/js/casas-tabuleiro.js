1feaa766340c4add14f92885484c80103fd15ec6
IvanFuziyama <ivanseiji21@gmail.com>
2026-08-30 11:51:39 -0300

    aparecendo a coordenada aleatorio no centro do tabuleiro

--- assets/js/casas-tabuleiro.js
+++ assets/js/casas-tabuleiro.js
@@ -1,5 +1,7 @@
 export const desenvolverCasas = e => {
     const tabuleiro = document.querySelector('.card-tabuleiro');
+    const arrayLetras = ['a','b','c','d','e','f','g','h'];
+    const arrayNumeros = [1,2,3,4,5,6,7,8];
     for(let bloco=0;bloco<8;bloco++){
         const col = document.createElement('div');
         for(let posicao=0;posicao<8;posicao++){
@@ -16,16 +18,30 @@ export const desenvolverCasas = e => {
                 const historico = document.querySelector('.pontuacao-historico')
                 casa.style.cursor = 'pointer';
                 casa.addEventListener('click', e =>{
-                    const arrayLetras = ['a','b','c','d','e','f','g','h'];
                     const numero = 8 - posicao;
-                    const letra = arrayLetras[bloco];
+                    const letra = arrayLetras[bloco]; //Pega o elemento da arrayLetras que está na posição indicada por bloco
                     historico.innerHTML += `${numero}${letra} `;
-                    console.log(posicao, bloco);
                 })
             }
+
             clicarCasas();
         }
         col.classList.add('col')
         tabuleiro.appendChild(col)
     }
+    const coordenadasRandom = e =>{
+        let numAleatorio = arrayNumeros[Math.floor(Math.random() * arrayNumeros.length)];
+        let letraAleatorio = arrayLetras[Math.floor(Math.random() * arrayLetras.length)];
+        let coordenadaCombinacao = letraAleatorio + numAleatorio;
+        const coordenadaExibida = document.createElement('div');
+        coordenadaExibida.textContent = coordenadaCombinacao;
+        tabuleiro.appendChild(coordenadaExibida);
+        coordenadaExibida.classList.add('coordenadaCentral')
+        coordenadaExibida.style.fontSize = '100px';
+        coordenadaExibida.style.color = 'blue';
+        coordenadaExibida.style.userSelect = 'none';
+        coordenadaExibida.style.pointerEvents = 'none';
+        console.log(coordenadaCombinacao);
+            }
+        coordenadasRandom();
 }
