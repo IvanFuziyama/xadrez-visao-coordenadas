f4b35b1cb878d1ef8ab0fcee048358389ce725e0
IvanFuziyama <ivanseiji21@gmail.com>
2026-09-13 15:58:17 -0300

    adicionando favicon e codificando o select das cores

---
+++ assets/js/select-cores.js
@@ -0,0 +1,11 @@
+// import { tabuleiro } from "./variaveis-global";
+export const alterarOrdem = e =>{
+        const select_cor = document.querySelector('.select-cor');
+                select_cor.addEventListener('change', e =>{
+                    if(select_cor.value === 'Branco'){
+
+                    }else if(select_cor.value === 'Preto'){
+                        
+                    }
+                })    
+            }
