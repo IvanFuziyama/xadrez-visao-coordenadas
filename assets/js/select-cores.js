9fd6d26aba374b2577fc0c5fae76d1aa9c49b4cf
IvanFuziyama <ivanseiji21@gmail.com>
2026-09-15 22:47:10 -0300

    alteração de cores pelo select funcionando

--- assets/js/select-cores.js
+++ assets/js/select-cores.js
@@ -1,15 +1,22 @@
 import { tabuleiro } from "./variaveis-global.js";
 export const alterarOrdem = e =>{
         const select_cor = document.querySelector('.select-cor');
+        const colunas = document.querySelectorAll('.col');
+        const numeros_lateral = document.querySelector('.coordenadasNum')
+        const letras_embaixo = document.querySelector('.coordenadasLetras')
                 select_cor.addEventListener('change', e =>{
                 if(select_cor.value === 'Branco') {
                     tabuleiro.style.flexDirection = 'row';
-                    colunas.forEach(col => {
+                    numeros_lateral.style.flexDirection = 'column';
+                    letras_embaixo.style.flexDirection = 'row';
+                    colunas.forEach(col => {  //o forEach percorre para cada elemento da array
                         col.style.flexDirection = 'column';
                     });
 
                 } else if(select_cor.value === 'Preto') {
                     tabuleiro.style.flexDirection = 'row-reverse';
+                    numeros_lateral.style.flexDirection = 'column-reverse';
+                    letras_embaixo.style.flexDirection = 'row-reverse';
                     colunas.forEach(col => {
                         col.style.flexDirection = 'column-reverse';
                     });
